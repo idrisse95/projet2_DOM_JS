2af9a76d5447b3665bd7a499ea599c1d45f2de3b
55973 <55973@etu.he2b.be>
2023-01-21 21:07:56 +0100

    hey hey a DEMAIN reste les point et temps restant

--- public/js/main.js
+++ public/js/main.js
@@ -436,7 +436,7 @@
 
 
 
-let carte = document.querySelectorAll('.card')
+let carte = document.querySelectorAll('#terrain .card')
 console.log(carte);
 
 let match = 0
@@ -473,7 +473,7 @@ function flipCarte(e) {
 }
 
 let back = document.querySelectorAll('#cards .card .back-view')
-console.log(back);
+
 // la je fait une function pour voir si ça match
 function memeCarte(srcImg1, srcImg2) {
   if (srcImg1 === srcImg2) {
@@ -538,6 +538,7 @@ function remettreCarte() {
 
   //en gros le pour le imgTag je prend l'index et je le remplace par le chiffre des src d img
   carte.forEach((element, index) => {
+  console.log(element);
     //faire reaparaitre les cartes
     element.style.opacity = "1"
     //enlever la class flip pour revoir le devant
@@ -566,12 +567,198 @@ console.log(match);
 
 
 
-// let input1 = document.getElementById('inputPseudo')
-// let chek = document.getElementById('chekInput')
 
-// let nomP = document.getElementById('nom')
 
-//  chek.addEventListener('click',()=>{
-//      nomP.innerHTML = input1.value
 
-// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let carte2 = document.querySelectorAll('#terrain2 #cards .card')
+console.log(carte2);
+let match2 = 0
+let carteUn2, carteD2;
+let desactive2 = false //Sa permet de desac les click apres carteUn et carteD
+//retourner la carte
+function flipCarte2(e) {
+  //cliquer sur la carte
+  let cliquer2 = e.target
+  //'!==' signifie different de ... et OU égale à
+  // dans le iff je dit qu'il prend en compte qu'un seul clique sur la meme carte
+  if (cliquer2 !== carteUn2 && !desactive2) {  
+    cliquer2.classList.add('flip')
+    //return la carte qui a té cliquer au premier clique
+
+    if (!carteUn2) {
+      return carteUn2 = cliquer2
+    }
+
+    carteD2 = cliquer2
+    //la les clique sont activer car on a pas encore ft de comparaison
+    desactive2 = true;
+    //une genre de comparaison des src puis seront appeler 
+    let premiereImg2 = carteUn2.querySelector('img').src
+    deuxiemeImg2 = carteD2.querySelector('img').src
+    //j'appel la fonction et je met en parametre les deux carte cliquer(leur src d'img)
+    memeCarte2(premiereImg2, deuxiemeImg2)
+
+  }
+
+
+
+}
+
+ 
+
+// la je fait une function pour voir si ça match
+function memeCarte2(srcImg12, srcImg22) {
+  if (srcImg12 === srcImg22) {
+    //Je donne 400ms avant d'executer SI les src son pareil
+    match2++
+    console.log(match2);
+    setTimeout(() => {
+    if (match2 == 4) {
+
+      setTimeout(() => {
+        return remettreCarte2()
+      }, 1000);
+      
+
+    }
+    
+      //les carte en opacity 0 pour quel soit fantome et donc ft en sorte que les autres gardent leur place
+      carteUn2.style.opacity = "0";
+      carteD2.style.opacity = "0";
+      carteUn2.removeEventListener('click', flipCarte2)
+      carteD2.removeEventListener('click', flipCarte2)
+      carteUn2 = carteD2 = "";
+    }, 400);
+
+
+
+
+
+
+
+
+    //on Desactive les clique sur les autre carte un lapse de temp (1 seconde, voir set timeout en bas)
+    return desactive2 = false;
+
+
+  }
+  //je met un timeOut pour dire après cmb de ms temp activer le else
+  setTimeout(() => {
+    carteUn2.classList.add('tourner')
+    carteD2.classList.add('tourner')
+
+  }, 400);
+
+  setTimeout(() => {
+    carteUn2.classList.remove("tourner", 'flip')
+    carteD2.classList.remove("tourner", 'flip')
+    carteUn2 = carteD2 = "" //la je dit qu'aprs mes deux clique, les variables se remettent a 0 pour éviter les bugg dans ancien clique enregistrer
+    desactive2 = false;
+  }, 1000);
+
+  return;
+
+}
+
+function remettreCarte2() {
+  match2 = 0
+  carteUn2 = carteD2 = "";
+  //je fait n tableau des valeur qui doivent etre matcher (log), pour mettre random tout (Le random j'ai chercher sur internet de base il n'etait pas comme ca. avec d modif ca a fonctionner mais sincerement j'y comprend pas 100%)
+  let tab2 = [1,2,3,4,1,2,3,4]
+  tab2.sort(() => Math.random() >0.5 ? 1 : -1)
+console.log(tab2);
+  //en gros le pour le imgTag je prend l'index et je le remplace par le chiffre des src d img
+  carte2.forEach((e, i) => {
+    console.log(e);
+    //faire reaparaitre les cartes
+    e.style.opacity = "1"
+    //enlever la class flip pour revoir le devant
+    e.classList.remove('flip')
+    let imgTag2 = e.querySelector('img')
+    imgTag2.src = `./public/img/img2/img${tab2[i]}.png`
+    e.addEventListener('click', flipCarte2)
+
+
+  });
+}
+
+remettreCarte2()
+
+carte2.forEach(e => {
+  e.addEventListener('click', flipCarte2)
+
+});
+
+console.log(match2);
+
+
+let total = document.getElementById('total')
+
+let point = document.getElementById('point')
+
+let terrain2 = document.getElementById('terrain2')
+let terrain = document.getElementById('terrain')
+
+let input1 = document.getElementById('inputPseudo')
+let chek = document.getElementById('chekInput')
+let acceuil = document.getElementById('acceuil') 
+let nomP = document.getElementById('pseudoKr')
+
+
+let niveau = document.getElementById('niveau')
+let moyen = document.getElementById('faci')
+let difficile = document.getElementById('diff')
+ chek.addEventListener('click',()=>{
+  if (input1.value == "") {
+    input1.style.border = "2px solid red"
+    input1.style.color = "gray"
+    alert('Mettez un pseudo')
+  }
+  else{
+     nomP.innerHTML += "   "+input1.value
+     acceuil.classList.add('fermer')
+     niveau.classList.add('ouvrir')
+    }
+
+})
+
+  moyen.addEventListener('click', ()=>{
+    niveau.classList.remove('ouvrir')
+    terrain2.classList.add('ouvrir','fantasy')
+    total.classList.add('fantasyBack')
+    point.classList.add('point')
+
+  })
+
+  difficile.addEventListener('click', ()=>{
+    niveau.classList.remove('ouvrir')
+    total.classList.add('horror')
+    point.classList.add('point')
+    terrain.classList.add('ouvrir')
+
+  })
